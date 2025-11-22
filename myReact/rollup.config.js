7831dee231189cfe0fa437b727573ac0c1f6f815
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-23 02:44:12 +0900

    feat: 빌드시 rollup 적용

---
+++ myReact/rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+
+export default {
+  input: 'src/core/lib.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      exports: 'named',
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+    }),
+  ],
+};
