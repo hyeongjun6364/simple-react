753599b00e950ca5ec6ddcd2450ddcd32feeeac2
Lim Hyeong Jun <100831607+hyeongjun6364@users.noreply.github.com>
2025-11-23 17:48:43 +0900

    refactor: 경로 및 네이밍 변경
    - rollup.config.js: 진입점 수정
    
    - package.json: name 변경

--- myReact/rollup.config.js
+++ myReact/rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 
 export default {
-  input: 'src/core/lib.js',
+  input: 'core/lib.js',
   output: [
     {
       file: 'dist/index.js',
